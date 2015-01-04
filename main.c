ede00347db59da3a75186928af78382c0af1bea5
Carl Poole <mail@carlpoole.com>
2015-01-04 10:37:31 -0500

    Added a couple of algo's to test with and flesh out program structure

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+void insertionSort(int a[], int array_size);
+void bubbleSort(int a[], int array_size);
+
 main(){
 	
 	printf("C Sorting Algorithm Speed Test\n");
@@ -16,6 +19,43 @@ main(){
 	
 	fclose(file);
 	
-	printf("%d %d %d\n",integers[50],integers[100],integers[45000]);
+	printf("Numbers Loaded\n");
 	
+	printf("%d %d %d %d %d\n",integers[0],integers[1],integers[2],integers[3],integers[4]);
+	
+	//insertionSort(integers, 50000);
+	bubbleSort(integers, 50000);
+	
+	printf("%d %d %d %d %d\n",integers[0],integers[1],integers[2],integers[3],integers[4]);
+	
+}
+
+void insertionSort(int a[], int array_size)
+{
+	 int i, j, index;
+	 for (i = 1; i < array_size; ++i)
+	 {
+		  index = a[i];
+		  for (j = i; j > 0 && a[j-1] > index; j--)
+			   a[j] = a[j-1];
+
+		  a[j] = index;
+	 }
+}
+
+void bubbleSort(int a[], int array_size)
+{
+	 int i, j, temp;
+	 for (i = 0; i < (array_size - 1); ++i)
+	 {
+		  for (j = 0; j < array_size - 1 - i; ++j )
+		  {
+			   if (a[j] > a[j+1])
+			   {
+					temp = a[j+1];
+					a[j+1] = a[j];
+					a[j] = temp;
+			   }
+		  }
+	 }
 }
